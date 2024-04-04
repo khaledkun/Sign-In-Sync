40fc509957d36017162ce3d04b436b32098deb9e
khaledsenpai <95447562+khaledkun@users.noreply.github.com>
2024-04-04 23:28:04 +0200

    Add files via upload

---
+++ Registration Form v10-K/function/login.php
@@ -0,0 +1,37 @@
+<?php
+$email = $_POST['email'];
+$password = $_POST['password'];
+// $username=$_POST['username'];
+// $password=md5($_POST['password']);
+require 'connect.php';
+$query = "SELECT * FROM employee WHERE email = '$email' AND password = '$password'";
+$result = $conn->query($query);// mysqli result object ==>current_field  ==> feild_count ==>  lengths ==>num_rows ==>type ==>
+var_dump($result);
+
+$user =$result->fetch_assoc();
+// foreach ($result as $row => $value) {
+// 	echo '<pre>';
+
+// 	print_r($value);
+
+// 	echo '</pre>';
+// }
+
+
+// echo '<pre>';
+// print_r($result);
+// echo '</pre>';
+
+$id = $user['id'];
+// echo $id;
+
+if ($result-> num_rows > 0) {
+	session_start();
+	$_SESSION['id']=$id;
+	echo $id;
+	header('location:../Html/index.php');
+
+	
+}else{
+	header('location:../Html/login.php');
+}
