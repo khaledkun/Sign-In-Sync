7403244d0bd1968482501d2b5893dfb19969ef8b
khaledHamam <khaledhamam920@gmail.com>
2024-04-11 17:03:29 +0200

    Config.phpعملت ملف ال
    وفيه تم تعريف جميع الصور في الموقع
    وتحويلها من مسار لمتغيرات
    بالتالي تسجل مره في الملف
    ولا تغير مره اخري

---
+++ Config.php
@@ -0,0 +1,20 @@
+<?php
+// No need to define a base path
+// because we are on localhost
+// and everyone has its own localhost
+
+// Define the directory containing the images
+$imgsPath = "/Shared/Imgs/";
+
+// Define paths for each image
+$employee1Path = $imgsPath . "Employee.png";
+$employee2Path = $imgsPath . "Employee.webp";
+$lockPath = $imgsPath . "lock.svg";
+$logoPath = $imgsPath . "Logo.ico";
+$mocap1Path = $imgsPath . "Mocap1-Y.jpg";
+$mocap2Path = $imgsPath . "Mocap2-Y.jpg";
+$navBarLogoPath = $imgsPath . "NavBarLogo.svg";
+$user1Path = $imgsPath . "User-Icon.png";
+$user2Path = $imgsPath . "user.svg";
+$websiteLogoPath = $imgsPath . "WebSiteLogo.svg";
+?>
--- Shared/Templates/MetaTags.php
+++ Shared/Templates/MetaTags.php
@@ -2,6 +2,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include "\Sign-In-Sync\Config.php"; ?>
 <title><?php echo $pageTitle; ?></title> <!-- Dynamic page title -->
 <!--stylesheets-->
 <link rel="icon" type="image/x-icon" href="/Shared/Imgs/WebSiteLogo.svg">
