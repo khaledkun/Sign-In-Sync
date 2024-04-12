1c02c1a7e251dd1500e8a20cfcb06648f4966933
khaledHamam <khaledhamam920@gmail.com>
2024-04-12 18:04:57 +0200

    تضبيط الأسماء

--- Shared/NavBar/DashboardNavFooter.php
+++ Shared/NavBar/DashboardNavFooter.php
@@ -17,7 +17,7 @@
         </div>
         <ul>
             <li><a href="BossDashboard.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "BossDashboard.php" ? 'class="active"' : ''; ?>><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-            <li><a href="Statistics.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "Statistics.php" ? 'class="active"' : ''; ?>><i class="fas fa-chart-bar"></i> Statistics</a></li>
+            <li><a href="BossStatistics.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "Statistics.php" ? 'class="active"' : ''; ?>><i class="fas fa-chart-bar"></i> Statistics</a></li>
             <li><a href="BossSettings.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "BossSettings.php" ? 'class="active"' : ''; ?>><i class="fas fa-cog"></i> Settings</a></li>
             <li><a href="../Dashboard/Html/login.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "login.php" ? 'class="active"' : ''; ?>><i class="fas fa-sign-out-alt"></i> Logout</a></li>
         </ul>
