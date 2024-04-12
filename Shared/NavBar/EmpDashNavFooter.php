7ab0c98754e949c6a933f194c8c218a4b4eb278c
SamehAyman <dtunbfok124@gmail.com>
2024-04-12 21:11:16 +0200

    شلت اخر بقايا الناف القديم

--- Shared/NavBar/EmpDashNavFooter.php
+++ Shared/NavBar/EmpDashNavFooter.php
@@ -17,7 +17,7 @@
         </div>
         <ul>
             <li><a href="EmployeeDashboard.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "EmployeeDashboard.php" ? 'class="active"' : ''; ?>><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-            <li><a href="EmployeeProfile.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "Profile.php" ? 'class="active"' : ''; ?>><i class="fas fa-user"></i> Profile</a></li>
+            <li><a href="EmployeeProfile.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "EmployeeProfile.php" ? 'class="active"' : ''; ?>><i class="fas fa-user"></i> Profile</a></li>
             <li><a href="EmployeeSettings.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "EmployeeSettings.php" ? 'class="active"' : ''; ?>><i class="fas fa-cog"></i> Settings</a></li>
             <li><a href="../Dashboard/Html/login.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "login.php" ? 'class="active"' : ''; ?>><i class="fas fa-sign-out-alt"></i> Logout</a></li>
         </ul>
