1b2c43503dff07dce8971ac37ad68458f96947c6
khaledHamam <khaledhamam920@gmail.com>
2024-04-13 13:42:48 +0200

    ضفت شويه تعليقات لبعدين

--- Shared/NavBar/EmpDashNavFooter.php
+++ Shared/NavBar/EmpDashNavFooter.php
@@ -21,10 +21,12 @@
             <li><a href="EmployeeSettings.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "EmployeeSettings.php" ? 'class="active"' : ''; ?>><i class="fas fa-cog"></i> Settings</a></li>
             <li><a href="../Dashboard/Html/login.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "login.php" ? 'class="active"' : ''; ?>><i class="fas fa-sign-out-alt"></i> Logout</a></li>
         </ul>
+        <!-- للدارك مود -->
         <div class="mode-toggle">
             <button id="dark-mode-btn"><i class="fas fa-moon"></i></button>
             <button id="light-mode-btn"><i class="fas fa-sun"></i></button>
         </div>
+        <!--  -->
         <div class="footer">
             <p>&copy; 2024 Sign In Sync</p>
         </div>
