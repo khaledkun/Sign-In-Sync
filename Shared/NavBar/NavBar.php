513d160e2b6c1d893b14e52494a13ccdb546b86d
SamehAyman <dtunbfok124@gmail.com>
2024-04-14 00:25:43 +0200

    Update NavBar.php

--- Shared/NavBar/NavBar.php
+++ Shared/NavBar/NavBar.php
@@ -22,7 +22,7 @@
             <label for="check" class="checkbox"><i class="ri-menu-line"></i></label>
             <input type="checkbox" name="check" id="check">
             <ul>
-                <li><a href="#Main">Home</a></li>
+                <li><a href="#Main" class="active">Home</a></li>
                 <li><a href="#Services">Services</a></li>
                 <li><a href="#About">About Us</a></li>
                 <li><a href="#Contact">Contact Us</a></li>
