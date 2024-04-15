8c62566f7dc4ca2da23192fee8550ca9ef0c3988
SamehAyman <dtunbfok124@gmail.com>
2024-04-15 04:24:26 +0200

    Update HomePage.php

--- Views/HomePage.php
+++ Views/HomePage.php
@@ -136,7 +136,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/661c01e0a0c6737bd12bab5e/1hremelga';
+s1.src='https://embed.tawk.to/661c8c731ec1082f04e2412a/1hrfo996a';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
