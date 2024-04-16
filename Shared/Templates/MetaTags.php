c78892ba7b4a6f9c1e294afe507e1666de6bd72c
khaledHamam <khaledhamam920@gmail.com>
2024-04-16 14:36:26 +0200

    بشتغل على فكرة ترتيب الملفات بشكل افضل
    لكن مغيرتش ترتيب اي ملفات

--- Shared/Templates/MetaTags.php
+++ Shared/Templates/MetaTags.php
@@ -2,7 +2,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php include "Config.php"; ?>
+<?php include "SharedConfig.php"; ?>
 <title><?php echo $pageTitle; ?></title> <!-- Dynamic page title -->
 <!--stylesheets-->
 <link rel="icon" type="image/x-icon" href="<?php echo $websiteLogoPath; ?>">
