432132a7ea25e2648ab1033657facb5df905623c
nada <nn1505696@gmail.com>
2024-04-22 11:16:33 +0200

    return the db local

--- Controllers/connect.php
+++ Controllers/connect.php
@@ -1,8 +1,16 @@
 <?php
-define('HOST', 'sql11.freesqldatabase.com');
-define('USERNAME', 'sql11698552');
-define('PASS','CbrDAHaavS');
-define('DB', 'sql11698552');
+// define('HOST', 'sql11.freesqldatabase.com');
+// define('USERNAME', 'sql11698552');
+// define('PASS','CbrDAHaavS');
+// define('DB', 'sql11698552');
+
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASS','');
+define('DB', 'sign in sync');
+
+
+
 
 $conn = new mysqli(HOST,USERNAME,PASS,DB);
 $conn->set_charset('utf8');
