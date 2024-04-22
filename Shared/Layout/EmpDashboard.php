1c749ad66322e56a6a187f1c66f4b154681b61f3
khaledHamam <khaledhamam920@gmail.com>
2024-04-22 19:09:03 +0200

    تجربه بس مش اكثر
    بعمل تيست

--- Shared/Layout/EmpDashboard.php
+++ Shared/Layout/EmpDashboard.php
@@ -7,6 +7,7 @@
     // Include the Template
     include '../Shared/Templates/MetaTags.php'; ?>
     <link rel="stylesheet" href="<?php echo $DashboardNavFooterPath; ?>">
+    <!-- <link rel="stylesheet" href="/Shared/Css/DashboardNavFooter-Test.Css"> -->
     <script src="<?php echo $DashLayout; ?>"></script>
 </head>
 <body>
