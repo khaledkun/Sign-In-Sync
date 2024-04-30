0243c44b3b268302896c20a95cb832fa18e3911c
nada <nn1505696@gmail.com>
2024-04-30 15:48:21 +0300

    some work in time zone

---
+++ Shared/Templates/Config.php
@@ -0,0 +1,40 @@
+<?php
+// No need to define a base path
+// because we are on localhost
+// and everyone has its own localhost
+
+// Define the directory containing the images
+$ImgsPath = "../Shared/Imgs/";
+
+// Define paths for each image
+$employee1Path = $ImgsPath . "Employee.png";
+$employee2Path = $ImgsPath . "Employee.webp";
+$lockPath = $ImgsPath . "lock.svg";
+$logoIcoPath = $ImgsPath . "Logo.ico";
+$mocap1Path = $ImgsPath . "Mocap1-Y.jpg";
+$mocap2Path = $ImgsPath . "Mocap2-Y.jpg";
+$navBarLogoPath = $ImgsPath . "NavBarLogo.svg";
+$user1Path = $ImgsPath . "user.svg";
+$user2Path = $ImgsPath . "User-Icon.png";
+$websiteLogoPath = $ImgsPath . "WebSiteLogo.svg";
+
+// Define the directory containing the Css
+$CssPath = "../Shared/Css/";
+
+// Define paths for each Css file
+$BossDashboardPath = $CssPath . "BossDashboard.css";
+$BossSettingsPath = $CssPath . "BossSettings.css";
+$StatisticsPath = $CssPath . "Statistics.css";
+$ProfilePath = $CssPath . "Profile.css";
+$SettingsPath = $CssPath . "Settings.css";
+$DashboardPath = $CssPath . "Dashboard.css";
+$ForgotPasswsordPath = $CssPath . "ForgotPasswsord.css";
+$HomepagePath = $CssPath . "Homepage.css";
+$CompanyVisionPath = $CssPath . "CompanyVision.css";
+$LoginPath = $CssPath . "Login.css";
+$MainStylePath = $CssPath . "MainStyle.css";
+$NavBarPath = $CssPath . "NavBar.css";
+$PrivacyPopUpPath = $CssPath . "PrivacyPopUp.css";
+$SignUpPath = $CssPath . "SignUp.css";
+$DashboardNavFooterPath = $CssPath . "DashboardNavFooter.Css";
+?>
