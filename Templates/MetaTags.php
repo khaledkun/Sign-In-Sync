a9ff0394bec84e4a3a9b7e3b601ad6f0c9611fdf
khaledHamam <khaledhamam920@gmail.com>
2024-05-10 19:18:48 +0300

    Update MetaTags.php
    ضفت ده في الميتا عشان لو انت احتجته تاني يكون موجود

--- Templates/MetaTags.php
+++ Templates/MetaTags.php
@@ -10,4 +10,5 @@
 <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
