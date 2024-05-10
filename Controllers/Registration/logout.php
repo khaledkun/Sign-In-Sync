4eae560fa29aceea6ce87e3ce06c456368913cc3
khaledHamam <khaledhamam920@gmail.com>
2024-05-10 18:58:58 +0300

    Update logout.php
    شغلت صفحه دي كمتغيرات

--- Controllers/Registration/logout.php
+++ Controllers/Registration/logout.php
@@ -2,5 +2,6 @@
 session_start();
 session_unset();
 session_destroy();
-
-header('location: ../../Views/Registration/login.php');
+include('../../Templates/SharedConfig.php');
+include($ConnectContrl);
+header('Location: ' . $LoginView);
