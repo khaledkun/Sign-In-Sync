807ece91647694a698b63f40231847092366ae5a
nada <nn1505696@gmail.com>
2024-05-13 12:13:32 +0300

    sitting boss controler done
    شغلت الجزء الاول من الsitting boss
    ناقص الجزأ التانى وهوه انى أعرض الأعدادات القديمه فى الviews قبل ميعدل المستخدم عليها

--- Controllers/Registration/login.php
+++ Controllers/Registration/login.php
@@ -4,7 +4,7 @@ $password = $_POST['password'];
 // $username=$_POST['username'];
 // $password=md5($_POST['password']);
 include('../../Templates/SharedConfig.php');
-include($ConnectContrl);
+include('connect.php');
 session_start();
 $query = "SELECT * FROM employee WHERE email = '$email' AND password = '$password'";
 $result = $conn->query($query);// mysqli result object ==>current_field  ==> feild_count ==>  lengths ==>num_rows ==>type ==>
@@ -36,33 +36,50 @@ if (!isset($_SESSION['first_login'])) {
 	// Otherwise, it's not the first login in this session
 	$_SESSION['first_login'] = false;
 }
-if ($result-> num_rows > 0 && $user['prive']==0) {
-	session_start();
-	$_SESSION['id']=$id;
-	
+//------------------------------------------------------------------login--php
+if($result-> num_rows > 0){
 
-	
-	header('Location: ' . $MydashboardView);
+	if ( $user['idPrive']==4 || $user['idPrive']==3) {
+		session_start();
+		$_SESSION['id']=$id;
+		header('Location: ' . $MydashboardView);
 
-	
-}elseif($result-> num_rows > 0 && $user['prive']==1){
-	session_start();
-	$_SESSION['id']=$id;
-	echo date_default_timezone_set('Asia/Riyadh');
-	$date = date('Y-m-d',time());
-	$time = date('H:i:s',time());
-	// echo $date."<br>";
-	// echo $time."<br>";
-	$query1 ="INSERT INTO login( id_empolyee, day, timee) VALUES ($id,'$date','$time')";
-	$result1 =$conn->query($query1);
-	// $query2 = "SELECT * FROM login"
+		
+	}elseif( $user['idPrive']==1 || $user['idPrive']==2){
+		session_start();
+		$_SESSION['id']=$id;
+		echo date_default_timezone_set('Asia/Riyadh');
+		$date = date('Y-m-d',time());
+		$time = date('H:i:s',time());
+		// echo $date."<br>";
+		// echo $time."<br>";
+		$query1 ="INSERT INTO login( id_empolyee, day, timee) VALUES ($id,'$date','$time')";
+		$result1 =$conn->query($query1);
+		// $query2 = "SELECT * FROM login"
 
-	// $id_login= $login['id_login'];
-	//type on anothor time will be work
-	$query3 ="INSERT INTO days (id_login, datee, type) VALUES ($id_login, $date, '1')";
+		// $id_login= $login['id_login'];
+		//type on anothor time will be work
+		$query3 ="INSERT INTO days (id_login, datee, type) VALUES ($id_login, $date, '1')";
+		// $result3=$conn->query($query3);
+		header('Location: ' . $MyEmpView);
+
+			// if(!$result2){
+	// 	header('location:fordatetable.php');
+	// }
+	// --------
+	
+	// $result2=$conn->query($query2);
+	// var_dump($result2);
+	// $logindata =$result2->fetch_assoc();
+
+	// $id_login= $logindata['id_login'];
+	// echo $id_login;
+	// //type on anothor time will be work
+	// $query3 ="INSERT INTO days (id_login, datee, type) VALUES ($id_login, $date, '1')";
 	// $result3=$conn->query($query3);
-	header('Location: ' . $MyEmpView);
-	
+	// header('location: ../Views/BossDashboard.php');
+		
+	}
 }else{
-	header('Location: ' . $LoginView);
+		header('Location: ' . $LoginView);
 }
--- Controllers/Registration/sittingboss.php
+++ Controllers/Registration/sittingboss.php
@@ -11,8 +11,9 @@ $HourShift0= $_POST['security0-2'];
 $maxAbsence0= $_POST['security0-3'];
 $maxLatencey0= $_POST['security0-4'];
 $payHour0= $_POST['security0-5'];
+//UPDATE `bosssitting` SET `idPrive`='[value-2]',`maxAbesence`='[value-3]',`maxLatence`='[value-4]',`salary`='[value-5]',`payHour`='[value-6]' WHERE 1
 
-$query ="UPDATE  bosssitting  SET    maxAbsence =$maxAbsence0, maxLatencey =$maxLatencey0, salary =$salary0, payHour =$payHour0 ,shiftHour= $HourShift0 WHERE id ='1'";
+$query ="UPDATE  bosssitting  SET  maxAbesence =$maxAbsence0 , maxLatence =$maxLatencey0 , salary =$salary0 , payHour =$payHour0 , shiftHour= $HourShift0 WHERE idPrive =1";
 $resulte = $conn->query($query);
 // $idPrive0 = $_POST['security0-1'];
 // $maxAbsence0= $_POST['security0-2'];
@@ -27,7 +28,9 @@ $maxAbsence1= $_POST['security1-3'];
 $maxLatencey1= $_POST['security1-4'];
 $payHour1= $_POST['security1-5'];
 
-$query1 ="UPDATE  bosssitting  SET    maxAbsence =$maxAbsence1, maxLatencey =$maxLatencey1, salary =$salary1, payHour =$payHour1 ,shiftHour= $HourShift1 WHERE idPrive =2";
+//$query ="UPDATE  bosssitting  SET  maxAbesence =$maxAbsence0 , maxLatence =$maxLatencey0 , salary =$salary0 , payHour =$payHour0 , shiftHour= $HourShift0 WHERE idPrive =1";
+
+$query1 ="UPDATE  bosssitting  SET    maxAbesence =$maxAbsence1, maxLatence =$maxLatencey1, salary =$salary1, payHour =$payHour1 ,shiftHour= $HourShift1 WHERE idPrive =2";
 $resulte1 = $conn->query($query1);
 //------------------------------------------------manager
 $salary2 = $_POST['security2-1'];
@@ -36,20 +39,14 @@ $maxAbsence2= $_POST['security2-3'];
 $maxLatencey2= $_POST['security2-4'];
 $payHour2= $_POST['security2-5'];
 
-$query2 ="UPDATE  bosssitting  SET    maxAbsence ='$maxAbsence2', maxLatencey ='$maxLatencey2', salary ='$salary2', payHour ='$payHour2' ,shiftHour= '$HourShift2' WHERE idPrive ='3'";
+$query2 ="UPDATE  bosssitting  SET    maxAbesence ='$maxAbsence2', maxLatence ='$maxLatencey2', salary ='$salary2', payHour ='$payHour2' ,shiftHour= '$HourShift2' WHERE idPrive =4";
 $resulte2 = $conn->query($query2);
-var_dump($resulte);
-echo "<br>";
-var_dump($resulte1);
-echo "<br>";
-var_dump($resulte2);
-echo "<br>";
-if($resulte){
-	echo '0done<br>';
-}
-if($resulte1){
-	echo '1done<br>';
-}
-if($resulte2){
-	echo '2done<br>';
+// var_dump($resulte);
+// echo "<br>";
+// var_dump($resulte1);
+// echo "<br>";
+// var_dump($resulte2);
+// echo "<br>";
+if($resulte && $resulte1 &&$ $resulte2){
+	header('location:../../views/Dashboard/MyDashboard.php');
 }
