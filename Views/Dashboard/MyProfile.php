b4db9e7534f9303be15fea664e552ddc98be24e8
khaledHamam <khaledhamam920@gmail.com>
2024-05-18 21:17:13 +0300

    Update MyProfile.php

--- Views/Dashboard/MyProfile.php
+++ Views/Dashboard/MyProfile.php
@@ -73,13 +73,10 @@ $imgpath = "../../Controllers/Registration/img/"."$imgname";
                     <input type="tel" id="phone" value="<?= $userdata1['phone']?>">
 
                     <label for="name"><i class="fa-solid fa-mars-and-venus Icons"></i> Gender :</label>
-                    <input type="text" id="name" value="<?= $userdata1['gender'] == 1? 'Femail':'Male'?>" disabled>
+                    <input type="text" id="name" value="<?= $userdata1['gender'] == 1? 'Female':'Male'?>" disabled>
 
                     <label for="name"><i class="fas fa-lock Icons"></i> Password :</label>
                     <input type="text" id="name" value="<?= $userdata1['passsword']?>" disabled><a href="<?php echo $ForgetPassView; ?>" target="_blank" class="change">Change</a>
-                    
-                    <label for="department"><i class="fas fa-building Icons"></i> Department :</label>
-                    <input type="text" id="department" value="IT" disabled>
                     
                     <label for="position"><i class="fas fa-briefcase Icons"></i> Position :</label>
                     <input type="text" id="position" value="<?php if($posstion ==1){ echo "Employee";} if($posstion ==2){ echo "Supervisor";} if($posstion ==3){ echo "Admin";} if($posstion ==4){ echo "Manager";}?>" disabled>
