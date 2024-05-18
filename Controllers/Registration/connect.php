e8e0b77df776e5da6bb0552599828ba7100fb256
khaledHamam <khaledhamam920@gmail.com>
2024-05-18 11:49:19 +0300

    Update connect.php

--- Controllers/Registration/connect.php
+++ Controllers/Registration/connect.php
@@ -4,6 +4,7 @@
 // define('PASS','CbrDAHaavS');
 // define('DB', 'sql11698552');
 
+// https://phpmyadmin.alwaysdata.com/
 define('HOST', 'mysql-sign-in-sync.alwaysdata.net');
 define('USERNAME', '359435');
 define('PASS','J@8L$wX9f!5K');
