defe2823220a6c77fea57844554532728758e2a7
SamehAyman7 <107861578+SamehAyman7@users.noreply.github.com>
2024-05-21 04:18:19 +0300

    اضافة الغاء تنشيط روابط الناف حسب صلاحيات الاكونت

--- Shared/Layout/EmpDashboard.php
+++ Shared/Layout/EmpDashboard.php
@@ -1,3 +1,17 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+$id =$_SESSION['id'];
+
+
+include '../../Templates/SharedConfig.php';
+include_once($ConnectContrl);
+$query1 ="SELECT * FROM employee WHERE id ='$id'";
+$result1=$conn->query($query1);
+$userdata1 =$result1->fetch_assoc();
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,7 +42,11 @@
             <li><a href="GeneralSettings.php" id="GeneralSettings" <?php echo basename($_SERVER['SCRIPT_NAME']) == "GeneralSettings.php" ? 'class="active"' : ''; ?>><i class="fas fa-cog"></i> General Settings</a></li>
             <li><a href="MyEmployees.php" id="MyEmployees" <?php echo basename($_SERVER['SCRIPT_NAME']) == "MyEmployees.php" ? 'class="active"' : ''; ?>><i class="fas fa-user-tie"></i> My Employees</a></li>
             <li><a href="EmployeesStatistics.php" id="EmployeesStatistics" <?php echo basename($_SERVER['SCRIPT_NAME']) == "EmployeesStatistics.php" ? 'class="active"' : ''; ?>><i class="fas fa-chart-bar"></i> Employees Statistics</a></li>
+            <?php if($userdata1['idPrive']==1 || $userdata1['idPrive']==2): ?>
             <li><a href="EmployeesSettings.php" id="EmployeesSettings" <?php echo basename($_SERVER['SCRIPT_NAME']) == "EmployeesSettings.php" ? 'class="active"' : ''; ?>><i class="fas fa-user-cog"></i> Employees Settings</a></li>
+            <?php elseif($userdata1['idPrive']==3 || $userdata1['idPrive']==4): ?>
+                <li><a href="EmployeesSettings.php" id="EmployeesSettings" style=" pointer-events: none;color: var(--Enabled);"><i class="fas fa-user-cog"></i> Employees Settings</a></li>
+        <?php endif; ?>
             <li><a href="../../Controllers/Registration/sessionwork.php" <?php echo basename($_SERVER['SCRIPT_NAME']) == "login.php" ? 'class="active"' : ''; ?>><i class="fas fa-sign-out-alt"></i> Logout</a></li>
         </ul>
         <!-- للدارك مود -->
